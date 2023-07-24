cd70ce05938e147d3672fec310c23ca18142a83b
JeffMayTA <jmay@tenadams.com>
2023-07-24 13:16:36 -0500

    before upgrading

--- my_application_package/blueprints/nuvance.py
+++ my_application_package/blueprints/nuvance.py
@@ -1,1 +1,4 @@
+from flask import Blueprint, render_template, redirect, url_for
+
+
 nuvance = Blueprint('nuvance', __name__)
--- my_application_package/models.py
+++ my_application_package/models.py
@@ -40,3 +40,10 @@ user_clients = db.Table('user_clients',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('client_id', db.Integer, db.ForeignKey('client.id'), primary_key=True)
 )
+
+class Role(db.Model):
+    __tablename__ = 'role'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    menu_items = db.relationship('MenuItem', secondary=role_menu_items, backref=db.backref('roles', lazy='dynamic'))
