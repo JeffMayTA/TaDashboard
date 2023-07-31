ab271afcd62f9cec476ecc1590d06c67523b8375
JeffMayTA <jmay@tenadams.com>
2023-07-31 17:22:17 -0500

    edits

--- my_application_package/dictionaries.py
+++ my_application_package/dictionaries.py
@@ -53,5 +53,54 @@ quotes_and_authors = [
     {
         "quote": "Believe you can and you're halfway there.",
         "author": "Theodore Roosevelt"
+    },
+    {
+        "quote": "The only way to do great work is to love what you do.",
+        "author": "Steve Wozniak"
+    },
+    {
+        "quote": "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        "author": "Winston Churchill"
+    },
+    {
+        "quote": "The future belongs to those who believe in the beauty of their dreams.",
+        "author": "Eleanor Roosevelt"
+    },
+    {
+        "quote": "In the midst of chaos, there is also opportunity.",
+        "author": "Sun Tzu"
+    },
+    {
+        "quote": "Don't watch the clock; do what it does. Keep going.",
+        "author": "Sam Levenson"
+    },
+    {
+        "quote": "The only place where success comes before work is in the dictionary.",
+        "author": "Vidal Sassoon"
+    },
+    {
+        "quote": "Life is what happens when you're busy making other plans.",
+        "author": "John Lennon"
+    },
+    {
+        "quote": "The best way to predict the future is to create it.",
+        "author": "Peter Drucker"
+    },
+    {
+        "quote": "The only limit to our realization of tomorrow will be our doubts of today.",
+        "author": "Franklin D. Roosevelt"
+    },
+    {
+        "quote": "If you can dream it, you can achieve it.",
+        "author": "Zig Ziglar"
+    },
+    {
+        "quote": "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+        "author": "Jordan Belfort"
+    },
+    {
+        "quote": "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+        "author": "Christian D. Larson"
     }
 ]
+
